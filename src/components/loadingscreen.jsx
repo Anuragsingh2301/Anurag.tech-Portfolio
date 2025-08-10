5941eb49634ba838d795ca85e3105a6c165ae2d5
Anuragsingh2301 <singh.an@somaiya.edu>
2025-08-11 00:37:41 +0530

    Slight Change

--- src/components/loadingscreen.jsx
+++ src/components/loadingscreen.jsx
@@ -6,7 +6,7 @@ import {Stars} from "@react-three/drei";
 export const LoadingScreen = ({iscomplete}) => {
     
     const[text,settext] = useState("");
-    const fulltext="Hello! I’m Anurag Singh";
+    const fulltext="Hello, my name is Anurag.";
     
 
     useEffect(()=>{
